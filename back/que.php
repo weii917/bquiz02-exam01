653d65889fd733751a3a7c42a168cdaf89210d30
wei <wendyhsu09@gmail.com>
2024-02-20 21:32:56 +0800

    修改問卷調查開放與關閉的問題

--- back/que.php
+++ back/que.php
@@ -24,42 +24,40 @@
 <!-- 14.6 問卷列表提供開放/關閉的功能，設為＂開放＂時，則可以在前台看到並進行投票；設為＂關閉＂時，則前台無法看到該份問卷．-->
 <fieldset>
     <legend>問卷列表 </legend>
-    <button onclick="location.href='?do=addnews'">新增文章</button>
-    <form action="./api/edit_news.php" method="post">
-        <table style="width: 75%;text-align:center;margin:auto">
+
+    <table style="width: 75%;text-align:center;margin:auto">
+        <tr>
+
+            <td class="clo" style="width:70%">問卷名稱</td>
+            <td class="clo">投票數</td>
+            <td class="clo">開放</td>
+        </tr>
+
+        <?php
+
+
+        $rows = $Que->all(['subject_id' => 0]);
+        foreach ($rows as $idx => $row) {
+        ?>
             <tr>
 
-                <td class="clo" style="width:70%">問卷名稱</td>
-                <td class="clo">投票數</td>
-                <td class="clo">開放</td>
+                <td><?= $row['text'] ?></td>
+                <td><?= $row['vote'] ?></td>
+                <td>
+                    <button class="show-btn" data-id="<?= $row['id']; ?>"><?= ($row['sh'] == 1) ? '開放' : '關閉'; ?></button>
+                    <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+
+                </td>
+
             </tr>
 
-            <?php
+
+        <?php
+        }
+        ?>
+    </table>
 
 
-            $rows = $Que->all(['subject_id' => 0]);
-            foreach ($rows as $idx => $row) {
-            ?>
-                <tr>
-
-                    <td><?= $row['text'] ?></td>
-                    <td><?= $row['vote'] ?></td>
-                    <td>
-                        <button class="show-btn" data-id="<?= $row['id']; ?>"><?= ($row['sh'] == 1) ? '開放' : '關閉'; ?></button>
-                        <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
-
-                    </td>
-
-                </tr>
-
-
-            <?php
-            }
-            ?>
-        </table>
-
-        <div class="ct"><input type="submit" value="修改確定"></div>
-    </form>
 </fieldset>
 <script>
     function more() {
